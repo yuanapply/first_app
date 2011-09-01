90cf6a59e6503999841f9487b344cfa6c4c591b8
Yuan Yuan <yuan@dhcp-172-19-16-122.mtv.corp.google.com>
2011-09-01 11:39:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c16e696327b27eb43c49b4e0f63d4df6a05f712b6919651985f3e3b8e5fc71d222c72e2f34757637e0f65bff463fdd7f67dd9703d35cdb6fc2d775fd00d8fdfa'
